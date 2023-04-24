896ccc460fcf4c192a02bbf79e394b5814e88f4e
cag <poorcag@protonmail.com>
2023-04-24 11:48:04 +1000

    text to speech

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response, JSONResponse
 
-from parsing import transcribe_from_audio, answer_my_question
+from parsing import transcribe_from_audio, answer_my_question, text_to_speech
 
 app = FastAPI()
 
@@ -29,8 +30,21 @@ async def upload_audio_file(audio_file: UploadFile = File(...)):
 
     answer = await answer_my_question(transcript)
 
-    return {
+    answer_audio = text_to_speech(answer)
+
+    # create a dictionary with the response data
+    response_data = {
         "filename" : audio_file.filename,
         "transcription": transcript,
-        "answer": answer
+        "answer": answer,
+        "file_size": len(answer_audio)
     }
+
+    # create a response object with the audio file and the response data
+    response = Response(content=answer_audio, media_type="audio/mp3", status_code=200)
+    response.headers["Content-Disposition"] = "attachment; filename=audio.mp3"
+    response.headers["filename"] = audio_file.filename
+    response.headers["transcription"] = transcript
+    response.headers["answer"] = answer
+    response.headers["file_size"] = str(len(answer_audio))
+    return response
--- parsing.py
+++ parsing.py
@@ -5,8 +5,15 @@
 # print(result["text"])
 
 import openai
+from elevenlabs import set_api_key, generate, play
 
 openai.api_key_path = '.key'
+
+eleven_api_key = ''
+with open('.elevenapikey') as f:
+    eleven_api_key = f.read()
+
+set_api_key(eleven_api_key)
 
 async def transcribe_from_audio(audio_file):
 
@@ -24,8 +31,9 @@ async def answer_my_question(question_text):
     response = await openai.ChatCompletion.acreate(
         model="gpt-3.5-turbo",
         messages=[
-                {"role": "system", "content": "You are a helpful kindergarden teacher."},
-                {"role": "user", "content": "I'd like to ask you a question. Please keep your answer short, simple, and accurate. Answer like you're talking to a primary school student."},
+                {"role": "system", "content": "You are a helpful primary school teacher."},
+                # {"role": "user", "content": "I'd like to ask you a question. You answer should be simple, accurate, and 2 sentences maximum. Answer kindly and politely like you're talking to a primary school student."},
+                {"role": "user", "content": "Answer my following question with 5 words maximum."},
                 {"role": "assistant", "content": "Of course, ask me anything you'd like!"},
                 {"role": "user", "content": question_text}
             ]
@@ -35,3 +43,11 @@ async def answer_my_question(question_text):
     output_message = response.get('choices')[0].get('message').get('content')
 
     return output_message
+
+
+def text_to_speech(text):
+
+    print (text)
+    audio = generate(text)
+    play(audio)
+    return audio
